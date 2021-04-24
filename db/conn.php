4fab28c260190bf021d1c96f05af0400bdfbf414
Anto <hahahasembarang@gmail.com>
2021-04-24 15:53:52 +0700

    upload ke 1

---
+++ db/conn.php
@@ -0,0 +1,32 @@
+<?php 
+    // Development Connection
+    // $host = 'localhost';
+    // $db = 'attendee_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';
+
+//    Remote Database Connection
+    $host = 'remotemysql.com';
+    $db = 'lWmL0drzpl';
+    $user = 'lWmL0drzpl';
+    $pass = 'cxxKv6y7i3';
+    $charset = 'utf8mb4';
+
+     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        throw new PDOException($e->getMessage());
+    }
+
+    require_once 'crud.php';
+    require_once 'user.php';
+    $crud = new crud($pdo);
+    $user = new user($pdo);
+   
+    $user->insertUser("admin","password");
+?>
